c4fd4f782b5af79e1f56205b775f325c7f4a89f1
cysneros@gmail.com <cysneros@gmail.com@fdd52888-2fd2-40d9-2659-c92be34f9d9a>
2012-04-25 19:02:34 +0000

    Initial Revision: Adding sample Omar client.  Client demonstrates how you can  get image KML files from Omar.

---
+++ trunk/OMAR/OmarClient/src/omarclient/ConfigConstants.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package omarclient;
+
+/**
+ *
+ * @author cysneros
+ */
+public interface ConfigConstants {
+    
+    	// ////////////////////////////////////////////
+	// String Values Expected in the config file
+	// ////////////////////////////////////////////
+	String m_sLog4jUrl = "";
+        public static final String sOmarServerURL= "omar.server.url";
+        
+    
+}
