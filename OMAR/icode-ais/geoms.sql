3419e2022bb3c74c3b1857b48f18ca5e6669c352
tsatsu.amable <tsatsu.amable@gmail.com>
2012-06-18 17:50:30 +0000

    initial import of my project

--- OMAR/icode-ais/geoms.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE location DROP COLUMN ais_geom;
-SELECT AddGeometryColumn( 'location', 'ais_geom', 4326, 'POINT', 2 );
-CREATE INDEX location_ais_geom_idx ON location USING GIST ( ais_geom GIST_GEOMETRY_OPS );
-
-VACUUM ANALYZE;
-
---
+++ build_cmake_project_vs10.bat
@@ -0,0 +1,26 @@
+@echo off
+
+set OLDPATH=%PATH%
+
+call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+
+set PATH="C:\Program Files\CMake 2.8\bin";%PATH%
+
+
+set common_dir="C:\development\3rd-party"
+set common_lib=%common_dir%/lib/win32
+set ossim_install_dir="C:\development\ossim_trunk"
+set ossim_lib_dir="C:\development\rapier\Release"
+set gdalDir="C:\development\gdal-1.9.0"
+
+rem set OPENCV_LIBRARIES=%common_lib%/cv210.lib;%common_lib%/cvaux210.lib;%common_lib%/cxcore210.lib;%common_lib%/cxts210.lib;%common_lib%/ml210.lib;%common_lib%/highgui210.lib
+
+cmake -G "Visual Studio 10"^
+ -DTHIRD_PARTY_INCLUDE_DIR=%common_dir%/include;%gdalDir%/include^
+ -DTHIRD_PARTY_LIBRARIES=%OPENCV_LIBRARIES%;%gdalDir%/lib/gdal_i.lib^
+ -DOSSIM_INCLUDE_DIR=%ossim_install_dir%/ossim/include;%ossim_install_dir%/ossim_plugins/gdal^
+ -DOSSIM_LIBRARIES=%ossim_lib_dir%/ossim.lib^
+ .
+
+set PATH=%OLDPATH%
+set OLDPATH=
