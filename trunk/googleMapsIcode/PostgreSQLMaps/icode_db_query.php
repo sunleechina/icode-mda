3d6e9997c47f6a89e8c04130f3f2c2d9a3722dac
mdasparta <mdasparta@fdd52888-2fd2-40d9-2659-c92be34f9d9a>
2013-04-09 15:13:22 +0000

    fixed bug: latmin, latmax, lonmin, lonmax -> minlat, maxlat, minlon, maxlon

--- trunk/googleMapsIcode/PostgreSQLMaps/icode_db_query.php
+++ trunk/googleMapsIcode/PostgreSQLMaps/icode_db_query.php
@@ -32,7 +32,12 @@ if (!$connection) {
    exit("Connection Failed: " . $conn);
 }
 
-$query = "SELECT target_location_id, report_date, message_source_id, message_type, ST_y(ST_AsText(location_data)) as Lat, ST_x(ST_AsText(location_data)) as Lon FROM target_location";
+//Original
+$query = "SELECT target_location_id, report_date, message_source_id, mmsi, message_type, ST_y(ST_AsText(location_data)) as lat, ST_x(ST_AsText(location_data)) as lon FROM target_location";
+
+//New query with unique MMSI
+//$query = "select distinct on (mmsi) mmsi, target_location_id, report_date, message_source_id, message_type, ST_y(ST_AsText(location_data)) as lat, ST_x(ST_AsText(location_data)) as lon  FROM target_location";
+
 if(count($_GET) > 0) //count the number of arguments
 {
    /*
@@ -50,12 +55,12 @@ if(count($_GET) > 0) //count the number of arguments
       else
          $query = $query . " where vesseltypeint=" .$type;
    }*/
-   if(!empty($_GET["latmin"]) && !empty($_GET["lonmin"]) &&
-      !empty($_GET["latmax"]) && !empty($_GET["lonmax"]))
+   if(!empty($_GET["minlat"]) && !empty($_GET["minlon"]) &&
+      !empty($_GET["maxlat"]) && !empty($_GET["maxlon"]))
    {
       $query = $query . " where ST_Intersects(location_data, ST_Transform( ST_MakeEnvelope(" .
-         $_GET["latmin"] . ", " . $_GET["lonmin"] . ", " . 
-         $_GET["latmax"] . ", " . $_GET["lonmax"] . ", 4326),4326) )";
+         $_GET["minlat"] . ", " . $_GET["minlon"] . ", " . 
+         $_GET["maxlat"] . ", " . $_GET["maxlon"] . ", 4326),4326) )";
    }
    if(!empty($_GET["limit"]))
    {
@@ -65,20 +70,34 @@ if(count($_GET) > 0) //count the number of arguments
 }
 else
 {
-   $limit = 5;
-   $query = "SELECT * FROM target_location limit " . $limit;
+   $limit = 25;
+   $query = "SELECT target_location_id, report_date, message_source_id, mmsi, message_type, ST_y(ST_AsText(location_data)) as lat, ST_x(ST_AsText(location_data)) as lon FROM target_location limit " . $limit;
+//   $query = "select mmsi, messagetype as message_type, mmsi as target_location_id, datetime as report_date, streamid as source, lat, lon from ter_20130402 limit " . $limit;
 }
 
+
 $result = @odbc_exec($connection, $query) or die('Query error: '.htmlspecialchars(odbc_errormsg()));;
+
 //$result = mysql_query($query);
 if (!$result) {
   die('Invalid query: ' . mysql_error());
 }
 
+
+
+
 header("Content-type: text/xml");
 
+echo '<?xml version="1.0" encoding="iso-8859-1"?>';
+
 // Start XML file, echo parent node
-echo '<tips>';
+//echo '<xml:query>';
+//echo '"' . $query . '"';
+//echo '</xml:query>';
+
+
+echo '<xml:result>';
+echo '<query statement="' . $query .'" />';
 // Iterate through the rows, printing XML nodes for each
 while (odbc_fetch_row($result)){
   // ADD TO XML DOCUMENT NODE
@@ -87,13 +106,14 @@ while (odbc_fetch_row($result)){
   echo 'report_date="' . odbc_result($result,"report_date") . '" ';
   echo 'message_source_id="' . odbc_result($result,"message_source_id") . '" ';
   echo 'message_type="' . odbc_result($result,"message_type") . '" ';
+  echo 'mmsi="' . odbc_result($result,"mmsi") . '" ';
   echo 'lat="' . odbc_result($result,"lat") . '" ';
   echo 'lon="' . odbc_result($result,"lon") . '" ';
   echo '/>';
 }
 
 // End XML file
-echo '</tips>';
+echo '</xml:result>';
 
 ?>
 
