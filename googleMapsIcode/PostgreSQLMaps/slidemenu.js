789d9e442b82b871b5ad5b21d7cfe11f0da4b33f
mdasparta <mdasparta@fdd52888-2fd2-40d9-2659-c92be34f9d9a>
2013-05-02 19:43:00 +0000

    Adding fading status message bar; adding sliding side panel; adding link to MarineTraffic from thumbnail; adding polyline drawing; showing MMSI if vesselname is null on mouseover

---
+++ googleMapsIcode/PostgreSQLMaps/slidemenu.js
@@ -0,0 +1,41 @@
+var slideDuration = 500;
+
+$(document).ready(function(){   
+   var $leftpanel = $('#panel_left');
+   var $leftwidth = $leftpanel.width();
+   var $mapcanvas = $('#map_canvas');
+   var $mapwidth = $mapcanvas.width();
+   var $showpanelbutton = $('#showpanel');
+
+   $leftpanel.click(function() {
+      togglePanel();
+   });
+   $showpanelbutton.click(function() {
+      togglePanel();
+   });
+
+   function togglePanel() {
+      $leftpanel.animate({
+         left: parseInt($leftpanel.css('left'),10) == 0 ? -($leftwidth-10) : 0
+      },
+      {duration: slideDuration, complete: function(){
+         $showpanelbutton.css('visibility',parseInt($leftpanel.css('left'),10) == 0 ? 'hidden' : 'visible');}
+      });
+
+      $mapcanvas.animate({
+         left: parseInt($mapcanvas.css('left'),10) == 0 ? -($leftwidth-10) : 0,
+         width: parseInt($mapcanvas.css('width'),10) == $mapwidth ? $mapwidth+($leftwidth-10) : $mapwidth
+         }, 
+         {duration: slideDuration, complete: function() {
+             map.setZoom( map.getZoom()+1 );
+             map.setZoom( map.getZoom()-1 );
+         }
+      });
+
+      $('#container').css('width', $mapwidth+500);
+   }
+
+   setTimeout(function() { togglePanel(); }, 3000);
+});
+
+
