df56e4253fa07cbcc3f07035890d321c7447c594
mdasparta <mdasparta@fdd52888-2fd2-40d9-2659-c92be34f9d9a>
2013-05-08 22:41:23 +0000

    Switching get vessel objects from XML to JSON objects

--- trunk/googleMapsIcode/PostgreSQLMaps/query_current_vessels.php
+++ trunk/googleMapsIcode/PostgreSQLMaps/query_current_vessels.php
@@ -6,16 +6,8 @@ $mtime = $mtime[1] + $mtime[0];
 $starttime = $mtime; 
 
 
-function parseToXML($htmlStr) { 
-   $xmlStr=str_replace('<','&lt;',$htmlStr); 
-   $xmlStr=str_replace('>','&gt;',$xmlStr); 
-   $xmlStr=str_replace('"','&quot;',$xmlStr); 
-   $xmlStr=str_replace("'",'&#39;',$xmlStr);
-   $xmlStr=str_replace("&",'&amp;',$xmlStr);
-   return $xmlStr; 
-} 
-
-
+//-----------------------------------------------------------------------------
+//Database execution
 //Keep database connection information secure
 require("phpsql_dbinfo.php");
 
@@ -38,40 +30,39 @@ if (!$connection) {
 
 //Query statement - default statement unless user inputs custom statement
 $query = "SELECT * FROM (SELECT * FROM radar_vessels UNION SELECT * FROM current_vessels WHERE vesseltypeint != -1) Q WHERE";
-//$query = "SELECT messagetype, mmsi, navstatus, rot, sog, lon, lat, cog, true_heading, datetime, imo, vesselname, vesseltypeint, length, shipwidth, bow, stern, port, starboard, draught, destination, callsign, posaccuracy, eta, posfixtype, streamid FROM current_vessels WHERE imo != -1";
+/*
+if(count($_GET) > 0) { 
+    if (!empty($_GET["keyword"])) {
 
-if(count($_GET) > 0) { //count the number of arguments
-    /*
-    if(!empty($_GET["type"])) {
-       $type = $_GET["type"];
-       if ($type == 999)    //catch all, display all case
-          $query = $query;
-       else if ($type == 70)
-          $query = $query . " and vesseltypeint>=70 and vesseltypeint <=79";
-       else if ($type == 80)
-          $query = $query . " and vesseltypeint>=80 and vesseltypeint <=89";
-       else if ($type == 60)
-          $query = $query . " and vesseltypeint>=60 and vesseltypeint <=69";
-       else
-          $query = $query . " and vesseltypeint=" .$type;
     }
-    */
-    if(!empty($_GET["minlat"]) && !empty($_GET["minlon"]) &&
+}
+*/
+
+//Count the number of arguments
+if(count($_GET) > 0) { 
+    if (!empty($_GET["minlat"]) && !empty($_GET["minlon"]) &&
        !empty($_GET["maxlat"]) && !empty($_GET["maxlon"])) {
-          //$query = $query . " AND lat > " . round($_GET["minlat"],3) . " and lon > " . round($_GET["minlon"],3) . " and lat < " .  round($_GET["maxlat"],3) . " and lon < " . round($_GET["maxlon"],3);
-          $query = $query . " lat BETWEEN " . round($_GET["minlat"],3) . " AND " . round($_GET["maxlat"],3) . " AND lon BETWEEN " .  round($_GET["minlon"],3) . " AND " . round($_GET["maxlon"],3);
+          //$query = $query . " AND lat > " . round($_GET["minlat"],3) . " and lon > " . round($_GET["minlon"],3) . 
+          //         " and lat < " .  round($_GET["maxlat"],3) . " and lon < " . round($_GET["maxlon"],3);
+          $query = $query . " lat BETWEEN " . round($_GET["minlat"],3) . " AND " . round($_GET["maxlat"],3) . 
+                   " AND lon BETWEEN " .  round($_GET["minlon"],3) . " AND " . round($_GET["maxlon"],3);
     }
-    if(!empty($_GET["keyword"])) {
+    if (!empty($_GET["keyword"])) {
        $keyword = $_GET["keyword"];
-       $query = $query . " AND (mmsi::varchar like ('%" . $keyword . "%') OR ". "imo::varchar like ('%" . $keyword . "%') OR vesselname::varchar ilike ('%" . $keyword . "%') OR destination::varchar ilike ('%" . $keyword . "%') OR callsign::varchar ilike ('%" . $keyword . "%') OR streamid::varchar ilike ('%" . $keyword . "%'))";
+       $query = $query . " AND (mmsi::varchar like ('%" . $keyword . "%') OR " . 
+                         "imo::varchar like ('%" . $keyword . "%') OR " . 
+                         "vesselname::varchar ilike ('%" . $keyword . "%') OR " . 
+                         "destination::varchar ilike ('%" . $keyword . "%') OR " . 
+                         "callsign::varchar ilike ('%" . $keyword . "%') OR " . 
+                         "streamid::varchar ilike ('%" . $keyword . "%'))";
     }
-    if(!empty($_GET["limit"])) {
+    if (!empty($_GET["limit"])) {
        $limit = $_GET["limit"];
        $query = $query . " limit " . $limit;
     }
 
     //custom query, erase everything else and use this query
-    if(!empty($_GET["query"])) {
+    if (!empty($_GET["query"])) {
        //TODO: add security checks, e.g. against "DROP TABLE *" commands
        $query = $_GET["query"];
     }
@@ -81,71 +72,64 @@ else {
     $query = $query . " limit " . $limit;
 }
 
-
 $result = @odbc_exec($connection, $query) or die('Query error: '.htmlspecialchars(odbc_errormsg()));;
-
-//$result = mysql_query($query);
-if (!$result) {
-    die('Invalid query: ' . mysql_error());
-}
+//-----------------------------------------------------------------------------
 
 
-
-/* Output XML */
-header("Content-type: text/xml");
-
-echo '<?xml version="1.0" encoding="iso-8859-1"?>';
-echo '<xml:result>';
-echo '<query statement="' . htmlspecialchars($query, ENT_NOQUOTES) .'" />';
-// Iterate through the rows, printing XML nodes for each
-$count_vessels = 0;
-while (odbc_fetch_row($result)){
-   $count_vessels = $count_vessels + 1;
-   // ADD TO XML DOCUMENT NODE
-   echo '<ais ';
-   echo 'messagetype="' . odbc_result($result,"messagetype") . '" ';
-   echo 'mmsi="' . odbc_result($result,"mmsi") . '" ';
-   echo 'navstatus="' . odbc_result($result,"navstatus") . '" ';
-   echo 'rot="' . odbc_result($result,"rot") . '" ';
-   echo 'sog="' . odbc_result($result,"sog") . '" ';
-   echo 'lon="' . addslashes(odbc_result($result,"lon")) . '" ';
-   echo 'lat="' . addslashes(odbc_result($result,"lat")) . '" ';
-   echo 'cog="' . odbc_result($result,"cog") . '" ';
-   echo 'true_heading="' . odbc_result($result,"true_heading") . '" ';
-   echo 'datetime="' . odbc_result($result,"datetime") . '" ';
-   echo 'imo="' . odbc_result($result,"imo") . '" ';
-   echo 'vesselname="' . htmlspecialchars(odbc_result($result,"vesselname")) . '" ';
-   echo 'vesseltypeint="' . odbc_result($result,"vesseltypeint") . '" ';
-   echo 'length="' . odbc_result($result,"length") . '" ';
-   echo 'shipwidth="' . odbc_result($result,"shipwidth") . '" ';
-   echo 'bow="' . odbc_result($result,"bow") . '" ';
-   echo 'stern="' . odbc_result($result,"stern") . '" ';
-   echo 'port="' . odbc_result($result,"port") . '" ';
-   echo 'starboard="' . odbc_result($result,"starboard") . '" ';
-   echo 'draught="' . odbc_result($result,"draught") . '" ';
-   echo 'destination="' . htmlspecialchars(odbc_result($result,"destination")) . '" ';
-   echo 'callsign="' . htmlspecialchars(odbc_result($result,"callsign")) . '" ';
-   echo 'posaccuracy="' . odbc_result($result,"posaccuracy") . '" ';
-   echo 'eta="' . odbc_result($result,"eta") . '" ';
-   echo 'posfixtype="' . odbc_result($result,"posfixtype") . '" ';
-   echo 'streamid="' . htmlspecialchars(odbc_result($result,"streamid")) . '" ';
-   echo '/>';
-}
-
-echo '<xml:resultCount>';
-echo '<resultcount count="' . $count_vessels . '" />';
-echo '</xml:resultCount>';
-
-echo '<xml:timetaken>';
+//End execution time
 $mtime = microtime(); 
 $mtime = explode(" ",$mtime); 
 $mtime = $mtime[1] + $mtime[0]; 
 $endtime = $mtime; 
 $totaltime = ($endtime - $starttime); 
-echo '<execution time="' . $totaltime . '" />';
-echo '</xml:timetaken>';
-echo '</xml:result>';
 
-// End XML file
+
+// Prevent caching.
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 01 Jan 1996 00:00:00 GMT');
+
+// The JSON standard MIME header.
+header('Content-type: application/json');
+
+//echo json_encode(array(query => $query));
+// Iterate through the rows, printing XML nodes for each
+$count_results = 0;
+$vesselarray = array();
+while (odbc_fetch_row($result)){
+   $count_results = $count_results + 1;
+
+   //Output JSON object per row
+   $vessel = array(messagetype=>odbc_result($result,"messagetype"),
+                   mmsi=>odbc_result($result,"mmsi"),
+                   navstatus=>odbc_result($result,"navstatus"),
+                   rot=>odbc_result($result,"rot"),
+                   sog=>odbc_result($result,"sog"),
+                   lon=>addslashes(odbc_result($result,"lon")),
+                   lat=>addslashes(odbc_result($result,"lat")),
+                   cog=>odbc_result($result,"cog"),
+                   true_heading=>odbc_result($result,"true_heading"),
+                   datetime=>odbc_result($result,"datetime"),
+                   imo=>odbc_result($result,"imo"),
+                   vesselname=>htmlspecialchars(odbc_result($result,"vesselname")),
+                   vesseltypeint=>odbc_result($result,"vesseltypeint"),
+                   length=>odbc_result($result,"length"),
+                   shipwidth=>odbc_result($result,"shipwidth"),
+                   bow=>odbc_result($result,"bow"),
+                   stern=>odbc_result($result,"stern"),
+                   port=>odbc_result($result,"port"),
+                   starboard=>odbc_result($result,"starboard"),
+                   draught=>odbc_result($result,"draught"),
+                   destination=>htmlspecialchars(odbc_result($result,"destination")),
+                   callsign=>htmlspecialchars(odbc_result($result,"callsign")),
+                   posaccuracy=>odbc_result($result,"posaccuracy"),
+                   eta=>odbc_result($result,"eta"),
+                   posfixtype=>odbc_result($result,"posfixtype"),
+                   streamid=>htmlspecialchars(odbc_result($result,"streamid"))
+   );
+   array_push($vesselarray, $vessel);
+}
+
+$data = array(query => $query, resultcount => $count_results, exectime => $totaltime, vessels => $vesselarray);
+echo json_encode($data);
 ?>
 
