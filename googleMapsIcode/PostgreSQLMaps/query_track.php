fedd50e249d157e2f45a36705473379067a42e02
mdasparta <mdasparta@fdd52888-2fd2-40d9-2659-c92be34f9d9a>
2013-05-08 21:35:08 +0000

    Switching get track objects from XML to JSON; moved ship details div; updated icons

--- googleMapsIcode/PostgreSQLMaps/query_track.php
+++ googleMapsIcode/PostgreSQLMaps/query_track.php
@@ -6,16 +6,16 @@ $mtime = $mtime[1] + $mtime[0];
 $starttime = $mtime; 
 
 
-function parseToXML($htmlStr) { 
-   $xmlStr=str_replace('<','&lt;',$htmlStr); 
-   $xmlStr=str_replace('>','&gt;',$xmlStr); 
-   $xmlStr=str_replace('"','&quot;',$xmlStr); 
-   $xmlStr=str_replace("'",'&#39;',$xmlStr);
-   $xmlStr=str_replace("&",'&amp;',$xmlStr);
-   return $xmlStr; 
-} 
+// Prevent caching.
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 01 Jan 1996 00:00:00 GMT');
+
+// The JSON standard MIME header.
+header('Content-type: application/json');
 
 
+//-----------------------------------------------------------------------------
+//Database execution
 //Keep database connection information secure
 require("phpsql_dbinfo.php");
 
@@ -37,8 +37,6 @@ if (!$connection) {
 }
 
 //Query statement
-//$query = "SELECT mmsi, lon, lat, datetime FROM (SELECT * FROM ter_20130401 UNION SELECT * FROM ter_20130402) A WHERE messagetype != 5 AND messagetype != 24 AND mmsi = ";
-//$query = "SELECT mmsi, lon, lat, datetime FROM (SELECT * FROM ter_20130401 UNION SELECT * FROM ter_20130402 UNION SELECT * FROM ter_20130403 UNION SELECT * FROM ter_20130404 UNION SELECT * FROM ter_20130405 UNION SELECT * FROM ter_20130406 UNION SELECT * FROM ter_20130407 UNION SELECT * FROM ter_20130408 UNION SELECT * FROM ter_20130409 UNION SELECT * FROM ter_20130410) A WHERE messagetype != 5 AND messagetype != 24 AND mmsi = ";
 if(!empty($_GET["mmsi"])) {
    if ($_GET["mmsi"] < 100) {
       $query = "SELECT mmsi, lon, lat, datetime FROM radar_20130415 A WHERE mmsi = ";
@@ -48,8 +46,9 @@ if(!empty($_GET["mmsi"])) {
    }
 }
 
-if(count($_GET) > 0) { //count the number of arguments
-    if(!empty($_GET["mmsi"])) {
+//Count the number of arguments
+if(count($_GET) > 0) {
+    if (!empty($_GET["mmsi"])) {
       $query = $query . $_GET["mmsi"];
     }
 
@@ -57,7 +56,10 @@ if(count($_GET) > 0) { //count the number of arguments
     /*
     if(!empty($_GET["minlat"]) && !empty($_GET["minlon"]) &&
        !empty($_GET["maxlat"]) && !empty($_GET["maxlon"])) {
-          $query = $query . " AND lat > " . $_GET["minlat"] . " and lon > " . $_GET["minlon"] . " and lat < " .  $_GET["maxlat"] . " and lon < " . $_GET["maxlon"];
+          $query = $query . " AND lat > " . $_GET["minlat"] . 
+                   " and lon > " . $_GET["minlon"] . 
+                   " and lat < " .  $_GET["maxlat"] . 
+                   " and lon < " . $_GET["maxlon"];
        }
      */
 
@@ -65,7 +67,7 @@ if(count($_GET) > 0) { //count the number of arguments
     $query = $query . " ORDER BY datetime DESC";
 
     //Add a limit if chosen
-    if(!empty($_GET["limit"])) {
+    if (!empty($_GET["limit"])) {
        $limit = $_GET["limit"];
        $query = $query . " LIMIT " . $limit;
     }
@@ -78,48 +80,32 @@ else {   //This case should not happen for track fetching (user must supply MMSI
 }
 
 $result = @odbc_exec($connection, $query) or die('Query error: '.htmlspecialchars(odbc_errormsg()));;
-
-if (!$result) {
-    die('Invalid query: ' . mysql_error());
-}
+//-----------------------------------------------------------------------------
 
 
-
-/* Output XML */
-header("Content-type: text/xml");
-
-echo '<?xml version="1.0" encoding="iso-8859-1"?>';
-echo '<xml:result>';
-echo '<query statement="' . htmlspecialchars($query, ENT_NOQUOTES) .'" />';
-// Iterate through the rows, printing XML nodes for each
-$count_results = 0;
-while (odbc_fetch_row($result)){
-   $count_results = $count_results + 1;
-
-   // ADD TO XML DOCUMENT NODE
-   echo '<ais ';
-   echo 'mmsi="' . odbc_result($result,"mmsi") . '" ';
-   echo 'lon="' . addslashes(odbc_result($result,"lon")) . '" ';
-   echo 'lat="' . addslashes(odbc_result($result,"lat")) . '" ';
-   echo 'datetime="' . odbc_result($result,"datetime") . '" ';
-   echo '/>';
-}
-
-echo '<xml:resultCount>';
-echo '<resultcount count="' . $count_results . '" />';
-echo '</xml:resultCount>';
-
-echo '<xml:timetaken>';
+//End execution time
 $mtime = microtime(); 
 $mtime = explode(" ",$mtime); 
 $mtime = $mtime[1] + $mtime[0]; 
 $endtime = $mtime; 
 $totaltime = ($endtime - $starttime); 
-echo '<execution time="' . $totaltime . '" />';
-echo '</xml:timetaken>';
 
-echo '</xml:result>';
+//echo json_encode(array(query => $query));
+// Iterate through the rows, printing XML nodes for each
+$count_results = 0;
+$vesselarray = array();
+while (odbc_fetch_row($result)){
+   $count_results = $count_results + 1;
 
-// End XML file
+   //Output JSON object per row
+   $vessel = array(mmsi=>odbc_result($result,"mmsi"),
+                   lat=>addslashes(odbc_result($result,"lat")),
+                   lon=>addslashes(odbc_result($result,"lon")),
+                   datetime=>odbc_result($result,"datetime")
+   );
+   array_push($vesselarray, $vessel);
+}
+
+$data = array(query => $query, resultcount => $count_results, exectime => $totaltime, vessels => $vesselarray);
+echo json_encode($data);
 ?>
-
