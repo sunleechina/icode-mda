0f44af78421dee218de05b834bf65214a6f6a582
mdasparta <mdasparta@fdd52888-2fd2-40d9-2659-c92be34f9d9a>
2013-12-20 01:00:14 +0000

    LAISIC tables fixes; added keyboard shortcuts on maps; other fixes

--- trunk/googleMapsIcode/ICODEMDAMaps/query_track.php
+++ trunk/googleMapsIcode/ICODEMDAMaps/query_track.php
@@ -33,6 +33,9 @@ if(count($_GET) > 0) {
    /* Check target source, start building the query */
    if(!empty($_GET["source"]) and (string)$_GET["source"] == "LAISIC_AIS_TRACK") {
       $query = "SELECT trkguid, updateguid, trknum, srcguid, datetime as TimeOfFix, lat as Latitude, lon as Longitude, cog, sog, stage, semimajor, semiminor, orientation, holdtime, hitscount, quality, source, inttype, callsign, mmsi, vesselname, imo FROM icodemaps.trackdata WHERE trknum=";
+      if (!empty($_GET["trknum"])) {
+         $query = $query . (string)$_GET["trknum"];
+      }      
    }
    else if(!empty($_GET["source"]) and (string)$_GET["source"] == "LAISIC_RADAR") {
       $query = "SELECT messagetype, mmsi, navstatus, rot, sog, lon as Longitude, lat as Latitude, cog, true_heading, datetime as TimeOfFix, imo, vesselname, vesseltypeint, length, shipwidth, bow, stern, port, starboard, draught, destination, callsign, posaccuracy, eta, posfixtype, streamid, target_status, target_acq, trknum, sourceid FROM icodemaps.radar_laisic_output WHERE trknum=";
@@ -58,6 +61,15 @@ if(count($_GET) > 0) {
    if (!empty($_GET["limit"])) {
       $limit = $_GET["limit"];
       $query = $query . " LIMIT " . $limit;
+   }
+
+   if (!empty($_GET["source"])) {
+      $source = $_GET["source"];
+   }
+
+   //For custom query (used in LAISIC tables)
+   if (!empty($_GET["query"])) {
+      $query = $_GET["query"];
    }
 }
 else {   //This case should not happen for track fetching (user must supply MMSI for query)
@@ -107,16 +119,19 @@ while (odbc_fetch_row($result)){
             );
       array_push($vesselarray, $vessel);
    }
-   else if ($source === "LAISIC_RADAR") {
+   else if ($source === "LAISIC_RADAR" || $source === "LAISIC_RADAR_HistoryTrail") {
       $vessel = array(mmsi=>odbc_result($result,"MMSI"),
                       lat=>addslashes(odbc_result($result,"Latitude")),
                       lon=>addslashes(odbc_result($result,"Longitude")),
                       datetime=>odbc_result($result,"TimeOfFix"),
                       sog=>odbc_result($result,"SOG"),
                       cog=>odbc_result($result,"COG"),
-                      streamid=>odbc_result($result,"RxStnID"),
-                      true_heading=>odbc_result($result,"Heading"),
-                      target_status=>odbc_result($result,"target_status")
+                      streamid=>odbc_result($result,"StreamID"),
+                      true_heading=>odbc_result($result,"true_heading"),
+                      target_status=>odbc_result($result,"target_status"),
+                      target_acq=>odbc_result($result,"target_acq"),
+                      trknum=>odbc_result($result,"trknum"),
+                      sourceid=>odbc_result($result,"sourceid")
             );
       array_push($vesselarray, $vessel);
    }
@@ -133,16 +148,40 @@ while (odbc_fetch_row($result)){
             );
       array_push($vesselarray, $vessel);
    }
-   else {
-      $vessel = array(mmsi=>odbc_result($result,"MMSI"),
+   else if ($source === "LAISIC_AIS_TRACK_HistoryTrail") {
+      $vessel = array(trkguid=>odbc_result($result,"trkguid"),
+                      updateguid=>odbc_result($result,"updateguid"),
+                      trknum=>odbc_result($result,"trknum"),
+                      srcguid=>odbc_result($result,"srcguid"),
+                      datetime=>odbc_result($result,"TimeOfFix"),
                       lat=>addslashes(odbc_result($result,"Latitude")),
                       lon=>addslashes(odbc_result($result,"Longitude")),
-                      datetime=>odbc_result($result,"TimeOfFix"),
-                      sog=>odbc_result($result,"SOG"),
                       cog=>odbc_result($result,"COG"),
-                      streamid=>odbc_result($result,"RxStnID"),
+                      sog=>odbc_result($result,"SOG"),
+                      stage=>odbc_result($result,"stage"),
+                      semimajor=>odbc_result($result,"semimajor"),
+                      semiminor=>odbc_result($result,"semiminor"),
+                      orientation=>odbc_result($result,"orientation"),
+                      holdtime=>odbc_result($result,"holdtime"),
+                      hitscount=>odbc_result($result,"hitscount"),
+                      quality=>odbc_result($result,"quality"),
+                      source=>odbc_result($result,"source"),
+                      inttype=>odbc_result($result,"inttype"),
+                      callsign=>odbc_result($result,"callsign"),
+                      mmsi=>odbc_result($result,"MMSI"),
+                      vesselname=>odbc_result($result,"vesselname"),
+                      imo=>odbc_result($result,"imo"),
+            );
+      array_push($vesselarray, $vessel);
+   }   
+   else {
+      $vessel = array(mmsi=>odbc_result($result,"MMSI"),
+                      datetime=>odbc_result($result,"TimeOfFix"),
+                      lat=>addslashes(odbc_result($result,"Latitude")),
+                      lon=>addslashes(odbc_result($result,"Longitude")),
+                      sog=>odbc_result($result,"SOG"),
                       true_heading=>odbc_result($result,"Heading"),
-                      target_status=>odbc_result($result,"target_status")
+                      streamid=>odbc_result($result,"RxStnID"),
             );
       array_push($vesselarray, $vessel);
    }
