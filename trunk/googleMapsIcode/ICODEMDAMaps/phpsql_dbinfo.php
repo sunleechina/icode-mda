47c250c9fec4d7ec799e16b8b8aa436e01ae569a
mdasparta <mdasparta@fdd52888-2fd2-40d9-2659-c92be34f9d9a>
2014-01-06 16:52:52 +0000

    Fixing PHP database login

--- trunk/googleMapsIcode/ICODEMDAMaps/phpsql_dbinfo.php
+++ trunk/googleMapsIcode/ICODEMDAMaps/phpsql_dbinfo.php
@@ -1,8 +1,9 @@
 <?php
 /* Connection data */
-$odbc_driver      = 'PostgreSQL Unicode(x64)';
-$odbc_database    = 'YOURTABLENAME';
-$odbc_host        = 'localhost';
-$odbc_user        = 'postgres';
+$odbc_driver      = 'MySQL ODBC 5.2 Unicode Driver';
+$ais_database     = 'icodemda';
+$laisic_database  = 'icodemaps';
+$odbc_host        = '127.0.0.1';
+$odbc_user        = 'YOURUSERNAME';
 $odbc_password    = 'YOURPASSWORD';
 ?>
