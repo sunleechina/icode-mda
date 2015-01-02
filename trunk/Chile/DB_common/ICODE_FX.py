0cdad93f61e07e4b561b56005c434daa0c249823
cwichel@gmail.com <cwichel@gmail.com@fdd52888-2fd2-40d9-2659-c92be34f9d9a>
2015-01-02 23:08:18 +0000

    Fixes: - Add function descriptions (Spanish) - Change histogram functions in ICODE_FX

--- trunk/Chile/DB_common/ICODE_FX.py
+++ trunk/Chile/DB_common/ICODE_FX.py
@@ -1,45 +1,96 @@
-###############################################################Libraries
-import sys, traceback
+#!/usr/bin/python2.7
+
+"""
+
+@file ICODE_FX.py
+@date Dic 2014 
+
+"""
+
+#-*- coding: utf-8 -*-
+
+# Libraries
+import sys
+import traceback
 from ICODE_DB import *
 from ICODE_RP import *
 
-#############################################################Definitions
-#--------------------------------------------------------seconds per day
+# Definitions
 spd = 86400
 
-##################################################################Makers		
+# Makers		
 DB = ICODE_DB()
 RP = ICODE_RP()
 
-#########################################################Final functions
+# Final functions
 #-----------------------------------------------------------------------
-def AIS_upload_DD(files, dform = '%Y-%m-%d %H:%M:%S.0', dbtable = 'vessel_history'):
+def AIS_upload_DB(files, formdate = '%Y-%m-%d %H:%M:%S.0', dbtable = 'vessel_history', coi = np.array(['mmsi','recvtime','latitude','longitude','sog','rot','rxstnid'])):
 	"""
-	Description: Upload formatted AIS data to a table. Note that needs an XLS or CSV file with 13 columns: MMSI, RecvTime, LocalRecvTime, MessageId, NavigationalStatus, ROT, SOG, COG, TrueHeading, PositionAccuracy, Longitude, Latitude, CRC.
-	The output will be a table with columns: MMSI, RecvTime, Latitude, Longitude, SOG, ROT.
-	Parameters:
-		- files: File with AIS data.					(type:string)
-		- dform: date format.							(type:string)
-		- dbtable: The name of reciever table.			(type:string)
+	Descripcion: Extrae y sube informacion AIS desde archivos XLS or CSV 
+	a la tabla indicada de una base de datos.
+	Los datos a subir se encuentran indicados por coi.
+	
+	Parametros:
+		- files: Archivo con datos AIS.                                 (type:string)
+		- formdate: Formato de fecha.                                   (type:string)
+		- dbtable: Nombre de la tabla de destino (BD).                  (type:string)
+		- coi: Columnas de interes a subir a la BD.                     (type:string)
 	"""
-	if isinstance(files, list) & DB.checkTableExists(dbtable):
-		for i in range(len(files)):
-			data = RP.AISfilter(RP.AIS2list(files[i]),dform)
-			data = np.take(data, [0,1,11,10,6,5], axis = 1)
-			data = np.hstack((data,np.zeros((len(data[:,0]),1))))
-			DB.uploadToDB(dbtable,data)
+	if isinstance(files, list):
+		if DB.checkTableExists(dbtable):
+			for i in range(len(files)):
+				data = RP.AIS2list(files[i], formdate, coi)
+				DB.uploadToDB(dbtable, data)
+		else:
+			print "*** ERROR: "+dbtable+" doesn't exists ***"
 	else:
-		print "*** ERROR: files has to be a list or table "+dbtable+" doesn't exists ***"
+		print "*** ERROR: files has to be a list or table ***"
 
 #-----------------------------------------------------------------------
-def AIS_date_hist(sdy, ndy = 0, bins = 1000, plim = [-90, 90,-180, 180], dform = '%Y-%m-%d', dbtable = 'vessel_history', folder = 'histograms'):
+def AIS_date_hist(sdy, ndy = 0, bins = 1000, plim = [-90, 90,-180, 180], formdate = '%Y-%m-%d', dbtable = 'vessel_history', folder = 'histograms'):
+	"""
+	Descripcion: Genera un heatmap para las posiciones de los barcos por
+	fecha y area geografica. Es creado un heatmap por dia, todos los 
+	histogramas son almacenados en un archivo bin de la forma:
+		#inicio del archivo
+		#informacion (sdy, ndy, xbins, ybins, plim)
+		#histograma dia 1
+		#histograma dia 2
+		#...
+		#histograma dia n
+		#fin del archivo
+	El nombre del archivo indica la fecha del primer histograma y el numero
+	de dias considerados luego del mismo.
+	
+	Parametros:
+		- sdy: Fecha del primer histograma. Puede ser UNIX.             (type:float/int/string)                             
+		- ndy: Numero de dias considerados para siguientes histogramas. (type:int)
+		- bins: Numero maximo de divisiones.                            (type:int) 
+		- plim: Limites geograficos [Latitud, Longitud].                (type:float)
+		- formdate: Formato de fecha.                                   (type:string)
+		- dbtable: Nombre de la tabla de la BD cual seran extraidos los
+		  datos.                                                        (type:string)
+		- folder: Nombre del directorio donde seran guardados los 
+		  histogramas.                                                  (type:string)
+		  
+	Ejemplo:
+		-> AIS_date_hist('2013-09-05', ndy = 5, bins = 1000, plim = [-23.11,-30.82,-113.43,-101.54])
+		Note que se realizaran 6 histogramas desde el 5 de septiembre
+		hasta el 10 de septiembre del 2013. La fecha pudo ser escrita 
+		tambien en formato UNIX: 1378353600, que puede ser escrito como
+		int o float. 
+		El area elegida para el histograma corresponde a un poligono que
+		cubre Isla de Pascua e Isla Sala y Gomez incluyendo su EEZ.
+		El archivo de salida tendra el nombre:
+			-> AIShist_2013-09-05_+5days.bin
+	"""
 	#For initial date: UNIX if int or float, else convert---------------
 	try:
 		sdy = float(sdy)
-		date = RP.unix2time(sdy,'%Y-%m-%d')
+		date = RP.unix2time(sdy, formdate)
 	except:											
 		date = sdy
-		sdy = RP.time2unix(sdy, dform)
+		sdy = RP.time2unix(sdy, formdate)
 		
 	#Use plim to set geo. limits. Note worldwide default----------------
 	plim = np.array(plim, dtype = float)
@@ -104,16 +155,33 @@ def AIS_date_hist(sdy, ndy = 0, bins = 1000, plim = [-90, 90,-180, 180], dform =
 
 #-----------------------------------------------------------------------
 def histfromfile(filename, ndy = 1):
+	"""
+	Descripcion: Permite extraer los histogramas generados por AIS_date_hist
+	del archivo .bin. Esta funcion tiene 2 salidas, la info de los 
+	histogramas y el histograma.
+	
+	Parametros:
+		- filename: Nombre del archivo .bin.                            (type:string)
+		- ndy: Numero del dia a extraer.                                (type:int)	
+	
+	Ejemplo:
+		-> histfromfile('AIShist_2013-09-05_+5days.bin', ndy = 2)
+		Se extraera el histograma correspondiente al 2do dia: 2013-09-06
+		
+	"""
+	#Open file
 	xfile = file(filename, 'rb')
 	finfo = np.load(xfile)
+	#If ndy is fine
 	if ndy > finfo[1]+1:
-		print 'Number of days stored exceeded, try with a lower ndy'
+		print '*** ERROR: Number of days stored exceeded, try with a lower ndy. ***'
 		xfile.close()
 		return finfo, None
 	elif ndy == 0:
-		print 'ndy has to be greater than 0'
+		print '*** ERROR: ndy has to be greater than 0. ***'
 		xfile.close()
 		return finfo, None
+	#Extract the hist
 	else:
 		for i in range(ndy):
 			out = np.load(xfile)
@@ -121,11 +189,87 @@ def histfromfile(filename, ndy = 1):
 		return finfo, out
 	
 #-----------------------------------------------------------------------
-def histplot(hist):
-		temp = np.ma.masked_where(hist == 0, hist)
-		pl.imshow(temp, cmap = pl.cm.hot)
-		pl.show()
-					
+def histplot(filename, hrange=[1,1], kmlout = 0, folder = 'kml_histograms'):
+	"""
+	Descripcion: Permite generar un histograma sobre un rango de dias en
+	base a un archivo .bin generado por AIS_date_hist.  
+	
+	Parametros:
+		- filename: Nombre del archivo .bin.                            (type:string)
+		- hrange: Rango de dias considerado para la suma de histogramas,
+		  el formato es [primer dia, ultimo dia].                       (type:int)
+		- kmlout: Si es 0, se muestra el histograma por pantalla, si es 
+		  1, se genera un archivo kml y una imagen para google earth.   (type:int)
+		- folder: Carpeta donde se guardaran los archivos kml en caso de
+		  generarlos, por defecto 'kml_histograms'.                     (type:string')
+	
+	Ejemplo:
+		-> histplot('AIShist_2013-09-05_+5days.bin', hrange = [1,2], kmlout = 1)
+		La linea anterior generara un histograma de la suma de los datos
+		del dia 1 y el dia 2. El kml de salida (y la imagen) tendran por
+		nombre '2013-09-05_day(1-2)', almacenadas en la carpeta 'kml_histograms'.
+	"""
+	#Open the hist file
+	xfile = file(filename, 'rb')
+	finfo = np.load(xfile)
+	#If hrange[0] is fine
+	if hrange[0] > finfo[1]+1:
+		print '*** ERROR: Initial range day exceeded the number od days, try with a lower one. ***'
+		xfile.close()
+	elif hrange[0] == 0:
+		print '*** ERROR: Initial range day has to be greater than 0. ***'
+		xfile.close()
+	else:
+		#hrange[1] has to be lower or equal to ndy
+		if hrange[1] > finfo[1]+1:
+			hrange[1] = finfo[1]+1
+		#Extract the needed histograms
+		count = 0
+		for i in range(int(finfo[1]+1)):
+			if i >= hrange[0] and i <= hrange[1]:
+				if count == 0:
+					hist = np.load(xfile)
+					count = 1
+				else:
+					hist += np.load(xfile)
+		xfile.close()
+		#Prepare hist to plot
+		hist = np.ma.masked_where(hist == 0, hist)
+		#If kmlout == 0, print the plot
+		if kmlout == 0:
+			pl.imshow(hist, cmap = pl.cm.hot)
+			pl.show()
+		#else, make a kml file to plot in google earth
+		else:
+			#filename
+			date = RP.unix2time(finfo[0], '%Y-%m-%d')
+			fname = '%s_day(%d-%d)' % (date,hrange[0],hrange[1])
+			#image
+			pl.imsave(RP.foldrv(folder)+fname+'.png',hist,cmap = pl.cm.hot)
+			#kml_file
+			xfile = file(RP.foldrv(folder)+fname+'.kml','w')
+			kml_file = """<?xml version="1.0" encoding="UTF-8"?>
+			<kml xmlns="http://www.opengis.net/kml/2.2">
+			<Folder>
+			<GroundOverlay>
+			<Icon>
+			<href>%s.png</href>
+			</Icon>
+			<LatLonBox>
+			<south>%f</south>
+			<north>%f</north>
+			<east>%f</east>
+			<west>%f</west>
+			<rotation>0</rotation>
+			</LatLonBox>
+			</GroundOverlay>
+			</Folder>
+			</kml>""" % (fname,finfo[4],finfo[5],finfo[6],finfo[7])
+			wait = xfile.write(kml_file)
+			xfile.close()
+				
+			
+
 
 
 
